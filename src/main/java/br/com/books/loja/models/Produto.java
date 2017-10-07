d8c40c6ebc6e315e23d4b78aecd920cdce0ddae5
Rielson-Leandro <rlsl@cin.ufpe.br>
2017-10-07 11:28:19 -0300

    Criação da classe produtos

---
+++ src/main/java/br/com/books/loja/models/Produto.java
@@ -0,0 +1,25 @@
+package br.com.books.loja.models;
+
+public class Produto {
+	private String titulo;
+	private String descricao;
+	private int paginas;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+}
